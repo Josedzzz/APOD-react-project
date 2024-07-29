719a97d25f080c51e3e2a92bc463c67652accd55
joki <josedavidamayar@gmail.com>
2024-07-28 23:53:11 -0500

    Remove serialization (bug)

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,11 @@ function App() {
   useEffect(() => {
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
-
-      const today = new Date().toDateString();
-      const localKey = `NASA-${today}`;
-
-      if (localStorage.getItem(localKey)) {
-        const apiData = JSON.parse(localStorage.getItem(localKey));
-        setData(apiData);
-        console.log("Fetched from cache today");
-        return;
-      }
-      localStorage.clear();
-
       try {
         const url =
           "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
         let res = await fetch(url);
         let apiData = await res.json();
-        localStorage.setItem(localKey, JSON.stringify(apiData));
         setData(apiData);
         console.log("Fetched from API today");
       } catch (err) {
